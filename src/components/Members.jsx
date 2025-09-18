f417ad0d94ee201f9c6a2266cf882a79f2fe4a70
Prachi <goyalprachi70150@gmail.com>
2025-09-18 11:40:10 +0530

    Initial commit

---
+++ src/components/Members.jsx
@@ -0,0 +1,15 @@
+function Members() {
+  return (
+    <div>
+      <h2>Members List</h2>
+      <ul>
+        <li>Prachi</li>
+        <li>Varshini</li>
+        <li>Tanish</li>
+        <li>Swaroop</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Members;
